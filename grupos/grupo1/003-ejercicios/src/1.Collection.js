52c77ef8348093b181fce02d180fa4a8cce070ba
German Nahuel Bogado <gerzbogado@gmail.com>
2020-07-03 18:18:09 -0300

    new comiiit updated not finished

--- grupos/grupo1/003-ejercicios/src/1.Collection.js
+++ grupos/grupo1/003-ejercicios/src/1.Collection.js
@@ -6,8 +6,9 @@ export class Collection{
         if(!this.has(elemento)) this.lista.push(elemento);
     }
     delete(elemento){
+        if(this.has(elemento)){
         let index = this.lista.indexOf(elemento);
-        if(index != -1) this.lista.splice(index,1);
+        if(index != -1) this.lista.splice(index,1);}
     }
     has(elemento){ return this.lista.includes(elemento)};
 }
