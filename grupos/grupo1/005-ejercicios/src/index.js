bdc432ae47c054416054179169c9f83943fd1df5
German Nahuel Bogado <gerzbogado@gmail.com>
2020-07-17 17:52:25 -0300

    ejercicio 5 con comentarios

--- grupos/grupo1/005-ejercicios/src/index.js
+++ grupos/grupo1/005-ejercicios/src/index.js
@@ -1,14 +1,27 @@
-import express from 'express';
+// importa metodos 
+import express, { json } from 'express';
 import bodyParser from 'body-parser';
-import alumnosRoutes from './controllers/alumnos';
+import alumnosRoutes from './Diplomatura Javascript/999 - Practicas/diplomatura.js/grupos/grupo1/005-ejercicios/src/routes/alumnosRouter';
+import profesoresRoutes from './Diplomatura Javascript/999 - Practicas/diplomatura.js/grupos/grupo1/005-ejercicios/src/routes/profesoresRouter';
+import { db } from './Diplomatura Javascript/999 - Practicas/diplomatura.js/grupos/grupo1/005-ejercicios/src/models/db';
 
+// definicion de puerto
 const PORT = 8080;
 const app = express();
+
+// conecta base de datos. el objeto db viene de los models
+db.connectDB((err,database )=>{
+   if (err) console.log(err);
+});
+
 app.use(bodyParser.json());
 
+// accede a los controladores creados
 app.use('/alumnos', alumnosRoutes);
+app.use('/profesores', profesoresRoutes);
 
-app.get('/', function (req, res) {
+// mensaje de bienvenida
+app.get('/', function (req,res) {
   res.json({ mensaje: 'Bienvenido al servidor de la Universidad' });
 });
 
---
+++ grupos/grupo1/005-ejercicios/src/routes/alumnosRouter.js
@@ -0,0 +1,24 @@
+import express from 'express';
+const router = express.Router();
+const alumnosController = require('../controllers/alumnosController');
+
+router.route('/')
+.post(
+    alumnosController.create
+)
+.get(
+    alumnosController.index
+)
+
+router.route('/:id')
+.get(
+    alumnosController.display
+)
+.put(
+    alumnosController.update
+)
+.delete(
+    alumnosController.destroy
+)
+
+module.exports = router;
